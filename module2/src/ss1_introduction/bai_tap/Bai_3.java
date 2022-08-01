430a6bc2a37beb121b7f7100dc6f7a74470a71c4
nguyenduyquang2712 <quangkg8@gmail.com>
2022-08-01 17:01:38 +0700

    ss1_introduction

---
+++ module2/src/ss1_introduction/bai_tap/Bai_3.java
@@ -0,0 +1,16 @@
+package ss1_introduction.bai_tap;
+
+import java.util.Scanner;
+
+public class Bai_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double vnd = 23000;
+        double usd;
+        System.out.print("Nhập số tiền cần chuyển đổi (USD): ");
+        usd = scanner.nextDouble();
+        double moneyCovert = usd*vnd;
+        System.out.print("Số tiền chuyển đổi là: "+moneyCovert +" VND");
+
+    }
+}
