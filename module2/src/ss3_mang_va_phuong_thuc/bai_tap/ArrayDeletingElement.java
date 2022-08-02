169dfc7121c9318457dab15022f90d1030a99df0
nguyenduyquang2712 <quangkg8@gmail.com>
2022-08-02 18:05:37 +0700

    ss2_vòng lặp

--- module2/src/ss3_mang_va_phuong_thuc/bai_tap/ArrayDeletingElement.java
+++ module2/src/ss3_mang_va_phuong_thuc/bai_tap/ArrayDeletingElement.java
@@ -10,29 +10,54 @@ public class ArrayDeletingElement {
         int amount = scanner.nextInt();
         int[] array = new int[amount];
         System.out.println("Nhập các phẩn tử mảng: ");
-        for (int i = 0; i < amount ; i++) {
+        for (int i = 0; i < amount; i++) {
             array[i] = scanner.nextInt();
         }
         System.out.println("Mảng khởi tạo là: ");
         System.out.println(Arrays.toString(array));
         System.out.println("Nhập phần tử muốn xóa: ");
         int number = scanner.nextInt();
-        for (int i = 0; i < array.length ; i++) {
-            if(number==array[i]){
-                System.out.println("phần tử cần xóa tại vị trí "+i);
-            }
-        }
-        for (int i = 0; i < array.length ; i++) {
-            if(number == array[i]){
-                for (int j = i; j < array.length-1 ; j++) {
-                    array[j] = array[j+1];
-
+        if (number == 0) {
+            int countNumber = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (number == array[i]) {
+                    System.out.println("phần tử cần xóa tại vị trí " + i);
+                    countNumber++;
                 }
-                array[array.length-1] = 0;
-                i--;
+            }
+            int countCheck=0;
+            for (int i = 0; i < array.length; i++) {
+                if (number == array[i]) {
+                    countCheck++;
+                    for (int j = i; j < array.length - 1; j++) {
+                        array[j] = array[j + 1];
+                    }
+                    array[array.length - 1] = 0;
+                    i--;
+                }
+                if (countCheck == countNumber) {
+                    break;
+                }
+            }
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (number == array[i]) {
+                    System.out.println("phần tử cần xóa tại vị trí " + i);
+                }
+            }
+            for (int i = 0; i < array.length; i++) {
+                if (number == array[i]) {
+                    for (int j = i; j < array.length - 1; j++) {
+                        array[j] = array[j + 1];
+
+                    }
+                    array[array.length - 1] = 0;
+                    i--;
+                }
             }
         }
         System.out.println("Mảng mới xóa phần tử là:");
         System.out.println(Arrays.toString(array));
+
     }
 }
