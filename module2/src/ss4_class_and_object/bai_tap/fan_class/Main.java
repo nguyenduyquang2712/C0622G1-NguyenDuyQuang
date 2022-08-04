95ff8ca31578f5612037c6487eeb959c604eb99a
nguyenduyquang2712 <quangkg8@gmail.com>
2022-08-04 20:38:37 +0700

    update

--- module2/src/ss4_class_and_object/bai_tap/fan_class/Main.java
+++ module2/src/ss4_class_and_object/bai_tap/fan_class/Main.java
@@ -3,12 +3,12 @@ package ss4_class_and_object.bai_tap.fan_class;
 public class Main {
     public static void main(String[] args) {
         Fan fan1 = new Fan();
-        fan1.setSpeed(3);
+        fan1.setSpeed(fan1.FAST);
         fan1.setRadius(10);
         fan1.setON(true);
         fan1.setColor("yellow");
         Fan fan2 = new Fan();
-        fan2.setSpeed(2);
+        fan2.setSpeed(fan2.MEDIUM);
         fan2.setRadius(5);
         fan2.setON(false);
         fan2.setColor("blue");
---
+++ module2/src/ss4_class_and_object/thuc_hanh/Example/Main.java
@@ -0,0 +1,7 @@
+package ss4_class_and_object.thuc_hanh.Example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SelectionSort.getVersion());
+    }
+}
