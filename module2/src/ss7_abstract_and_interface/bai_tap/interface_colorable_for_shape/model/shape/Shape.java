8ab062e4a9ce8dc20d75d01cf36153b4d1227560
nguyenduyquang2712 <quangkg8@gmail.com>
2022-08-09 17:03:56 +0700

    update

--- module2/src/ss7_abstract_and_interface/bai_tap/interface_colorable_for_shape/model/shape/Shape.java
+++ module2/src/ss7_abstract_and_interface/bai_tap/interface_colorable_for_shape/model/shape/Shape.java
@@ -2,40 +2,40 @@ package ss7_abstract_and_interface.bai_tap.interface_colorable_for_shape.model.s
 
 
 public abstract class Shape {
-//    private String color = "green";
-//    private boolean filled = true;
-//
-//    public Shape() {
-//    }
-//
-//    public Shape(String color, boolean filled) {
-//        this.color = color;
-//        this.filled = filled;
-//    }
-//
-//    public String getColor() {
-//        return color;
-//    }
-//
-//    public void setColor(String color) {
-//        this.color = color;
-//    }
-//
-//    public boolean isFilled() {
-//        return filled;
-//    }
-//
-//    public void setFilled(boolean filled) {
-//        this.filled = filled;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "A Shape with color of "
-//                + getColor()
-//                + " and "
-//                + (isFilled() ? "filled" : "not filled");
-//    }
+    private String color = "green";
+    private boolean filled = true;
+
+    public Shape() {
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "A Shape with color of "
+                + getColor()
+                + " and "
+                + (isFilled() ? "filled" : "not filled");
+    }
 
 
 }
